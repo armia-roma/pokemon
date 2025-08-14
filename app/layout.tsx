17c266c877ad793aee40e2a04b7abca0faf06033
Armia Habib <armiagorge249@gmail.com>
2025-08-14 12:22:28 +0400

    feat: add QueryProvider for React Query integration

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,8 @@ import type {Metadata} from "next";
 import {Geist, Geist_Mono} from "next/font/google";
 import "./globals.css";
 import ChakraProvider from "./ChakraProvider";
+import QueryProvider from "./QueryProvider";
+
 const geistSans = Geist({
 	variable: "--font-geist-sans",
 	subsets: ["latin"],
@@ -27,7 +29,9 @@ export default function RootLayout({
 			<body
 				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
 			>
-				<ChakraProvider>{children}</ChakraProvider>
+				<QueryProvider>
+					<ChakraProvider>{children}</ChakraProvider>
+				</QueryProvider>
 			</body>
 		</html>
 	);
